9bb0f6231987d801cc1a1a0fd33764f55bfb2ac4
Michael Yankura <myankura210@gmail.com>
2019-04-15 11:33:21 -0500

    completed fire-employees exercise

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
-
+// import employee from "./employee.jpg"
+import "./employee.css"
 
 export default class EmployeeList extends Component {
-    render() {
+    render () {
         return (
-            <section className="content">
-            <h3>Our Team</h3>
+            <section className="content employee">
             {
                 this.props.employees.map(employee =>
-                    <div key={employee.id}>
-                        {employee.name}
-                        <hr></hr>
+                    <div key={employee.id} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                {employee.name}
+                                <button
+                                    onClick={() => this.props.deleteEmployee(employee.id)}
+                                    className="card-link">Delete</button>
+                            </h5>
+                        </div>
                     </div>
                 )
             }
