2a8f835eb607014e5e7b9fb5748e814c67c6a3aa
Michael Yankura <myankura210@gmail.com>
2019-04-15 10:54:40 -0500

    added the ability to delete animals from the API.

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react'
+import dog from "./DogIcon.svg"
+import "./animal.css"
 
 export default class AnimalList extends Component {
-    render() {
-        return(
-        <section className="content">
-            <h3>Our Animals</h3>
-                {
-                    this.props.animals.map(animal =>
-                        <div key={animal.id}>
-                            {animal.name}
-                            <hr></hr>
+    render () {
+        return (
+            <section className="content animals">
+            {
+                this.props.animals.map(animal =>
+                    <div key={animal.id} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                <img src={dog} className="icon--dog" alt="dog"/>
+                                {animal.name}
+                                <button
+                                    onClick={() => this.props.deleteAnimal(animal.id)}
+                                    className="card-link">Delete</button>
+                            </h5>
                         </div>
-                    )
-                }
-                </section>
+                    </div>
+                )
+            }
+            </section>
         )
     }
 }
+// export default class AnimalList extends Component {
+//     render() {
+//         return(
+//         <section className="content">
+//             <h3>Our Animals</h3>
+//                 {
+//                     this.props.animals.map(animal =>
+//                         <div key={animal.id}>
+//                             {animal.name}
+//                             <hr></hr>
+//                         </div>
+//                     )
+//                 }
+//                 </section>
+//         )
+//     }
+// }
