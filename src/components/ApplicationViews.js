28183beb26ab9196e4d626c67f580e10cbc03e07
Michael Yankura <myankura210@gmail.com>
2019-04-16 10:44:29 -0500

    Completed resource manager exercise. Did some minor clean up of the code.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -87,28 +87,3 @@ export default class ApplicationViews extends Component{
         )
     }
 }
-
-fetch("http://localhost:5002/animals")
-            .then(r => r.json())
-
-
-
-
-
-
-
-
-
-            // fetch("http://localhost:5002/animals")
-            // .then(r => r.json())
-            // .then(animals => newState.animals = animals)
-            // .then(() => fetch("http://localhost:5002/employees")
-            // .then(r => r.json()))
-            // .then(employees => newState.employees = employees)
-            // .then(() => fetch("http://localhost:5002/locations")
-            // .then(r => r.json()))
-            // .then(locations => newState.locations = locations)
-            // .then(() => fetch("http://localhost:5002/owners")
-            // .then(r => r.json()))
-            // .then(owners => newState.owners = owners)
-            // .then(() => this.setState(newState))
