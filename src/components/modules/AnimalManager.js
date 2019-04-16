c17ea7e94ef91ccef3af72001e3d4cdd9006fd79
Michael Yankura <myankura210@gmail.com>
2019-04-16 10:42:32 -0500

    All resource managers is complete.

---
+++ src/components/modules/AnimalManager.js
@@ -0,0 +1,11 @@
+const remoteURL = "http://localhost:5002"
+
+//responsible for getting all of the animals from the API
+export default {
+  get(id) {
+    return fetch(`${remoteURL}/animals/${id}`).then(e => e.json())
+  },
+  getAll() {
+    return fetch(`${remoteURL}/animals`).then(e => e.json())
+  }
+}
